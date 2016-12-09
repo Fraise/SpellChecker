207a22307f44e6ddf6ca470dee12463056bd229f
Fraise <n0valyfe@hotmail.fr>
2016-12-09 16:06:25 +0100

    Corrected a few pointers, now correctly highlight non finished words.

--- words_functions.c
+++ words_functions.c
@@ -56,10 +56,14 @@ void add_word(treeNode *baseNode, char *word)
 		if ((*baseNode).ptrArray[char_to_index(*word)] == NULL)
 		{
 			(*baseNode).ptrArray[char_to_index(*word)] = calloc(1, sizeof(treeNode));
+			if (*(word + 1) == '\0')
+				(*((*baseNode).ptrArray[char_to_index(*word)])).end = 1;
 			add_word((*baseNode).ptrArray[char_to_index(*word)], word + 1);
 		}
 		else
 		{
+			if (*(word + 1) == '\0')
+				(*((*baseNode).ptrArray[char_to_index(*word)])).end = 1;
 			add_word((*baseNode).ptrArray[char_to_index(*word)], word + 1);
 		}
 	}
@@ -67,17 +71,17 @@ void add_word(treeNode *baseNode, char *word)
 
 int check_word(treeNode *baseNode, char *word)
 {
-	//int word_ok;
 	if (*word != '\0')
 	{
-		//printf("checking letter : %c\n", *word);
-		if ((*baseNode).ptrArray[char_to_index(*word)] != NULL)
+		if ((*(word + 1) == '\0') && ((*baseNode).ptrArray[char_to_index(*word)] != NULL) && (((*((*baseNode).ptrArray[char_to_index(*word)])).end == 1)))
+			return 1;
+		else if ((*baseNode).ptrArray[char_to_index(*word)] != NULL)
 			return check_word((*baseNode).ptrArray[char_to_index(*word)], word + 1);
 		else
 			return 0;
 	}
 	else
-		return 1;
+		return 0;
 }
 
 void free_tree(treeNode *baseNode)
